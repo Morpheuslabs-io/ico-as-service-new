a59a933e7a1660f8d32b6f532b1e6c402a2b148b
trung <>
2019-02-01 19:12:33 +0700

    Store user request and response into sqlite

--- script/sqlite.js
+++ script/sqlite.js
@@ -10,6 +10,8 @@ var SqliteHandler = {
       await db.runAsync("CREATE TABLE IF NOT EXISTS address1 (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT)");
 
       await db.runAsync("CREATE TABLE IF NOT EXISTS address2 (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT)");
+
+      await db.runAsync("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, request TEXT, response TEXT)");
       
       console.log('loadDB OK');
     } catch (err) {
@@ -75,6 +77,34 @@ var SqliteHandler = {
       console.log('predeployed ' + addressTableName + ' Error: ', err);
       return null;
     }
+  },
+  async pushUserRequest(requestData) {
+
+    let requestStr = JSON.stringify(requestData.request);
+
+    let queryInsert = "INSERT INTO " + "user" + " (email, request) VALUES (";
+    queryInsert += "'" + requestData.email + "'" + "," + "'" + requestStr + "'" + ")";
+
+    try {
+      await db.runAsync(queryInsert);
+      console.log('pushUserRequest OK - email: ' + requestData.email);
+    } catch (err) {
+      console.log('pushUserRequest Error: ' + err);
+    }
+  },
+  async pushUserResponse(responseData) {
+
+    let responseStr = JSON.stringify(responseData.response);
+
+    let queryInsert = "INSERT INTO " + "user" + " (email, response) VALUES (";
+    queryInsert += "'" + responseData.email + "'" + "," + "'" + responseStr + "'" + ")";
+
+    try {
+      await db.runAsync(queryInsert);
+      console.log('pushUserResponse OK - email: ' + responseData.email);
+    } catch (err) {
+      console.log('pushUserResponse Error: ' + err);
+    }
   }
 }
 
@@ -93,7 +123,44 @@ var SqliteHandler = {
 //   await SqliteHandler.close();
 // }
 
-// test();
+async function test2() {
+  let requestData = {};
+  requestData.email = 'midotrinh@gmail.com';
+  requestData.request = {
+    step2: {
+      name: 'Morpheus',
+      ticker: 'MMM',
+      decimals: 18,
+      reserved_token: []
+    },
+    step3: {
+      wallet_address: '0x8847f80cb1b2b567679b3166a0c828453e122c7f',
+      email_address: 'midotrinh@gmail.com',
+      gasPrice: '3.01',
+      mincap: 1,
+      enableWhitelisting: 'no',
+      tiers: 
+      [ { sequence: 1,
+          tierName: 'Tier 1',
+          allowModifying: 'no',
+          startDate: '2019-02-01',
+          startTime: '15:27:45',
+          endDate: '2019-02-02',
+          endTime: '15:27:45',
+          lockDate: '2019-02-03T08:27:16.454Z',
+          unlockDate: '2019-02-05T08:27:16.454Z',
+          rate: 10,
+          supply: 1000000,
+          whitelist: [] } ]
+    }
+  };
+
+  await SqliteHandler.loadDB('./database/sqlite.db');
+  await SqliteHandler.pushUserRequest(requestData);
+  await SqliteHandler.close();
+}
+
+test2();
 
 module.exports = SqliteHandler;
 
