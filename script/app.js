535e7641e661a95a2f10f591ec9f1e6925363af7
trung <>
2019-03-04 16:13:46 +0700

    Fix port

--- script/app.js
+++ script/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use('/', express.static('public_static'));
 app.use(cors());
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => {
   console.log('Server is running on port ', port);
